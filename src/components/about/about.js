185b3fb56daf632407b79a3c32f13ab0945acd82
OliviaVF <opvf22@gmail.com>
2017-05-14 18:57:46 +0100

    changed home layout and 'about'

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,18 +10,18 @@ export default class About extends React.Component {
         <div className="about" id="page-wrap">
           <h1>About Me</h1>
           <div className="words">
-            <p>After completing a degree in Philosophy, I spent 3 and a half years working for various start ups in wide-reaching, operational roles. I ended up project managing several tech builds which led to a fascination with development.</p>
+            <p>After completing a degree in Philosophy, I spent three and a half years working for various start ups in wide-reaching, operational roles. I ended up project managing several tech builds which led to a fascination with development.</p>
             <p>I decided to take the General Assembly immersive web development course because of my desire to start doing the building myself, no longer wanting to look from the outside in.</p>
             <p>My brain is creative and scientific in equal measures, and web development truly satisfies all the different ways I like to think and do.</p>
             <p>I am currently looking to join a relatively small, friendly, dynamic team as a full stack developer, building something that positively affects society.</p>
           </div>
 
-          <a href="https://github.com/OliviaVF" target="_blank">
-            <div className="github"></div>
-          </a>
-
           <a href="https://www.linkedin.com/in/olivia-vaughan-fowler/" target="_blank">
             <div className="linkedin"></div>
+          </a>
+
+          <a href="https://github.com/OliviaVF" target="_blank">
+            <div className="github"></div>
           </a>
 
           <a href="mailto:opvf22@gmail.com">
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,9 +5,8 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 export default class Layout extends React.Component {
   render() {
     return (
-      <div className="container" id="outer-container">
+      <div id="outer-container">
         <div className="home" id="page-wrap">
-          <div className="home"></div>
         </div>
       </div>
     );
